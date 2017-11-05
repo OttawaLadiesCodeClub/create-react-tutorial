d0b9fc9edc985e498bed26e9f8a58367e78dbfeb
RACHEL HARTVIKSEN <rhartviksen@gmail.com>
2017-11-05 14:42:52 -0500

    Add static react mock up of event list

--- src/App.js
+++ src/App.js
@@ -4,15 +4,58 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Meetup_Logo.png/250px-Meetup_Logo.png" className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Ottawa Ladies Code Club</h1>
-        </header>
-        <p className="App-intro">
-          Hello (React) world.
-        </p>
-      </div>
+        <div className="App">
+          <header className="App-header">
+              <img src="http://flaticons.net/icons/Mobile%20Application/Calendar-Date-04.png" class="App-logo" alt="logo" />
+              <h1 className="App-title">Upcoming engagements</h1>
+          </header>
+          <p className="App-intro">
+            <div>
+                <form>
+                  <input type="text" placeholder="Search..."/>
+                  <p><input type="checkbox" value="on"/> Only show future events</p>
+                </form>
+                <table>
+                  <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Date</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                        <td>Functional vs Object Oriented Programming</td>
+                        <td>2017-06-12</td>
+                      </tr>
+                      <tr>
+                        <td>Pushing left Talk</td>
+                        <td>2017-07-13</td>
+                      </tr>
+                      <tr>
+                        <td>Regex Talk</td>
+                        <td>2017-08-14</td>
+                      </tr>
+                      <tr>
+                        <td>White paper club - Excluded Middle</td>
+                        <td>2017-11-01</td>
+                      </tr>
+                      <tr>
+                        <td>React Tutorial</td>
+                        <td>2017-11-09</td>
+                      </tr>
+                      <tr>
+                        <td>Ladies in Code in History</td>
+                        <td>2017-12-06</td>
+                      </tr>
+                      <tr>
+                        <td>Security Leadership Talk</td>
+                        <td>2017-01-04</td>
+                      </tr>
+                  </tbody>
+                </table>
+            </div>
+          </p>
+        </div>
     );
   }
 }
