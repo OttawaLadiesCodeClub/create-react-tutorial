add2daa4ad53278ddabf3f07a37103112443d72f
RACHEL HARTVIKSEN <rhartviksen@gmail.com>
2017-11-05 13:59:41 -0500

    Change out of box App component to an OLCC hello world

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Meetup_Logo.png/250px-Meetup_Logo.png" className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Ottawa Ladies Code Club</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello (React) world.
         </p>
       </div>
     );
