ad29eb46f7c18fc0e1647d2ad51272a917b2ec31
hpkent <hpkent@gmail.com>
2017-11-06 19:45:36 -0500

    adding data for react components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const EVENTS = [
+      {date: '2017-06-12', upcoming: false, name: 'Functional vs Object Oriented Programming'},
+      {date: '2017-08-14', upcoming: false, name: 'Pushing left Talk'},
+      {date: '2017-11-01', upcoming: false, name: 'White paper club - Excluded Middle'},
+      {date: '2017-11-09', upcoming: false, name: 'React Tutorial'},
+      {date: '2017-12-06', upcoming: true, name: 'Ladies in Code in History'},
+      {date: '2017-01-04', upcoming: true, name: 'Security Leadership Talk'}
+    ];
     return (
         <div className="App">
           <header className="App-header">
